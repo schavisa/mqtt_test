d0f71692bf5e238f30165a6c38bbc4ced0a64cfe
Adam Smith <adamrsmith98@gmail.com>
2023-02-13 13:03:05 -0700

    Fixed date selection

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Define the map centered on Calgary
 var map = L.map('map').setView([51.039439, -114.054339], 11);
 
+// Add the basemap
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -9,7 +11,7 @@ $(function() {
     $('input[name="daterange"]').daterangepicker({
         opens: 'right'
     }, async (start, end, label) => {
-        const response = await fetch("https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate > '" + start.format('YYYY-MM-DD') + "' and issueddate < '" + end.format('YYYY-MM-DD') + "'");
+        const response = await fetch("https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate >= '" + start.format('YYYY-MM-DD') + "' and issueddate <= '" + end.format('YYYY-MM-DD') + "'");
         const geojson = await response.json();
         console.log(geojson);
     });
