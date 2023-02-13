bf03369154eaade38ce8e62206e359915765109e
Adam Smith <adamrsmith98@gmail.com>
2023-02-13 12:13:35 -0700

    Add search function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.039439, -114.054339], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+$(function() {
+    $('input[name="daterange"]').daterangepicker({
+        opens: 'left'
+    }, async (start, end, label) => {
+        const response = await fetch("https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate > '" + start.format('YYYY-MM-DD') + "' and issueddate < '" + end.format('YYYY-MM-DD') + "'");
+        const geojson = await response.json();
+        console.log(geojson);
+    });
+});
+
