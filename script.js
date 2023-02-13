11fd32afe44d35155c1f52b1ea3af163f218a945
Adam Smith <adamrsmith98@gmail.com>
2023-02-13 12:35:03 -0700

    Update styling.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 $(function() {
     $('input[name="daterange"]').daterangepicker({
-        opens: 'left'
+        opens: 'right'
     }, async (start, end, label) => {
         const response = await fetch("https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate > '" + start.format('YYYY-MM-DD') + "' and issueddate < '" + end.format('YYYY-MM-DD') + "'");
         const geojson = await response.json();
